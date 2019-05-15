978ac9f86edbcb4e4d51c0eb9490b575558846b6
prakriti <2016pietcevishakh116@poornima.org>
2019-05-15 18:14:28 +0530

    Second commit

---
+++ Hello.py
@@ -0,0 +1,12 @@
+print("Hello there""")
+a = "my name"
+print(a)
+print(a[1])
+print(a[1:5])
+b = "the name of the name is a name"
+print(b.replace("name","thing"))
+print(a.split("n"))
+print(a)
+print("enter your name")
+c = input()
+print("so",c)
